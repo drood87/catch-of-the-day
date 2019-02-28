25fa55891a0607d3060fe6da9d728e23f5988a0b
drood <dbernstein1987@hotmail.com>
2019-02-28 20:17:59 +1100

    header/inventory/order components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom'; // just pick one method from react-dom instead of whole package
 import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css';
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
