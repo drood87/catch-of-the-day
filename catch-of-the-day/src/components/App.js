04c4040e6443faf687150ce9830e0563781bf16a
drood <dbernstein1987@hotmail.com>
2019-03-04 17:26:00 +1100

    fish components props update

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish key={key} details={this.state.fishes} />
             ))}
           </ul>
         </div>
