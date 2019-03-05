cecacef4bce4bc26065d5b97ca3e153984331c09
drood <dbernstein1987@hotmail.com>
2019-03-05 18:29:26 +1100

    loading data in local storage

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,7 +5,9 @@ class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    // Make sure the fish is loaded before we continue
+    if (!fish) return null; // if we return null it will render out absolutely nothing
 
     if (!isAvailable) {
       return (
